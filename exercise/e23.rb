1cb838da73bed65a0172b574c2e309ca3ae9f7b6
Anshika <anshika2020ag@gmail.com>
2025-09-12 14:37:24 +0530

    csv parser

--- exercise/e23.rb
+++ exercise/e23.rb
@@ -2,33 +2,57 @@
 
 require 'csv'
 
-# converting csv table to hash
-class CSV
-  def self.to_hash(file_path)
-    hashh = {}
-    foreach(file_path, headers: true) do |row|
-      designation = row[' Designation'].strip
-      name = row['Name'].strip
-      empid = row[' EmpId'].strip
+class Employee
+  attr_reader :name, :empid, :designation
 
-      desig = hashh[designation] ||= []
+  def initialize(empid, name, designation)
+    @empid = empid
+    @name = name
+    @designation = designation
+  end
 
-      desig << [name, empid]
-    end
-    hashh.sort_by { |_, v| v }.to_h
+  def to_s
+    "#{@name} (EmpId: #{@empid})\n"
   end
 end
 
-# printing from hash
-class Hash
-  def print_formatted
-    each_pair do |desig, pair|
-      puts desig
-      pair.each { |val| puts "#{val[0]} (EmpId: #{val[1]})" }
+class List
+  def initialize
+    @employees = []
+  end
+
+  def add_employee(emp)
+    @employees << emp
+  end
+
+  def group_by_designation
+    @employees.group_by { |emp| emp.designation }.sort_by { |_, val| -val.size}.to_h
+  end
+
+  def print_list
+    group_by_designation.each_pair do |key, val|
+      puts val.size > 1 ? "#{key}s" : key
+      val.each { |emp| puts emp }
       puts
     end
   end
 end
 
+# converting CSV table to hash
+class CSV
+
+  def self.append_to_list(file_path, list)
+    foreach(file_path, headers: true) do |row|
+      designation = row[' Designation'].strip
+      name = row['Name'].strip
+      empid = row[' EmpId'].strip
+      list.add_employee(Employee.new(empid, name, designation))
+    end
+  end
+
+end
+
 file_path = 'C:\Users\anshi\Desktop\ruby\csv\employee_data.csv'
-CSV.to_hash(file_path).print_formatted
+list = List.new
+CSV.append_to_list(file_path, list)
+list.print_list
