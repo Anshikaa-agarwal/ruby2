6d0ffe12d8f1f0b9058fe55e9920d6e06dd3ca13
Anshika <anshika2020ag@gmail.com>
2025-09-26 11:02:56 +0530

    added retry feature and filename, line no to instance_eval

--- advance/exercise/dynamic_class.rb
+++ advance/exercise/dynamic_class.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# methods to take inputs
+# module to take input
 module Inputs
   def input(msg)
     print msg
@@ -8,11 +8,17 @@ module Inputs
   end
 
   def input_class_name
-    input('Please enter the class name: ')
+    class_name = input('Please enter the class name: ')
+    raise ArgumentError, 'class name must start with uppercase' unless class_name[0] =~ /[A-Z]/
+
+    class_name
   end
 
   def input_method_name
-    input('Please enter the method name you wish to define: ')
+    method_name = input('Please enter the method name: ')
+    raise ArgumentError, 'method name must not start with uppercase' if method_name[0] =~ /[A-Z]/
+
+    method_name
   end
 
   def input_method_code
@@ -20,11 +26,12 @@ module Inputs
   end
 end
 
+# included module input
 class Object
   include Inputs
 end
 
-# create class dynamically
+# class to generate class dynamically
 class DynamicClass
   attr_reader :my_class
 
@@ -33,22 +40,39 @@ class DynamicClass
   end
 
   def def_method(method_name, method_body)
-    my_class.define_method(method_name) do
-      instance_eval(method_body)
+    @my_class.define_method(method_name) do
+      instance_eval(method_body, 'dynamic_class.rb', 44)
     end
   end
 
   def call(method_name)
-    puts "Hello, Your class #{my_class} with method #{method_name} is ready."
+    puts "Hello, your class #{my_class} with method #{method_name} is ready."
     puts "Calling: #{my_class}.new.#{method_name}:"
     my_class.new.send(method_name)
   end
 end
 
-class_name  = input_class_name
-method_name = input_method_name
+begin
+  class_name = input_class_name
+rescue ArgumentError => e
+  puts "#{e.message} (#{e.class})"
+  retry
+end
+
+begin
+  method_name = input_method_name
+rescue ArgumentError => e
+  puts "#{e.message} (#{e.class})"
+  retry
+end
+
 method_code = input_method_code
 
 my_class = DynamicClass.new(class_name)
 my_class.def_method(method_name, method_code)
-my_class.call(method_name)
+
+begin
+  my_class.call(method_name)
+rescue StandardError => e
+  puts "#{e.message} (#{e.class})"
+end
