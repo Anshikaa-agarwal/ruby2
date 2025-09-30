d8b8fe1b142021c9b163e74d0d0683862bc545f8
Anshika <anshika2020ag@gmail.com>
2025-09-30 11:43:01 +0530

    added Item class and method to compare string, sTriNG

--- advance/exercise/shopping_list.rb
+++ advance/exercise/shopping_list.rb
@@ -3,6 +3,31 @@
 # out of bound quantity
 class NegativeQuantity < StandardError; end
 
+# class Item with name
+class Item
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    @name
+  end
+
+  def ==(other)
+    other.is_a?(Item) && name.downcase == other.name.downcase
+  end
+
+  def eql?(other)
+    self == other
+  end
+
+  def hash
+    name.downcase.hash
+  end
+end
+
 # class shopping list with methods to access it
 class ShoppingList
   attr_accessor :list
@@ -12,13 +37,15 @@ class ShoppingList
   end
 
   def add(item, quantity)
+    item = make_item(item)
     list[item] += quantity
   end
 
   def remove(item, quantity)
+    item = make_item(item)
     if (list[item] - quantity).negative?
       list[item] = 0
-      raise NegativeQuantity, "Warning: Not enough #{item}s"
+      raise NegativeQuantity, "Warning: Not enough #{item.name}s"
     else
       list[item] -= quantity
     end
@@ -29,19 +56,28 @@ class ShoppingList
   end
 
   def to_s
-    @list.map { |item, quantity| "#{item}: #{quantity}" }.join("\n")
+    @list.map { |item, quantity| "#{item.name}: #{quantity}" }.join("\n")
+  end
+
+  private
+
+  def make_item(obj)
+    obj.is_a?(Item) ? obj : Item.new(obj.to_s.capitalize)
   end
 end
 
+# usage
 s1 = ShoppingList.new
+
 begin
   s1.items do
+    add('toothpaste', 2) 
+    add('Computer', 1) 
+    add('tOothpaste', 2) 
     remove('Book', 2)
-    add('Toothpaste', 2)
-    add('Computer', 1)
   end
-rescue
-  puts ($!).message
+rescue NegativeQuantity => e
+  puts e.message
 end
 
 puts s1
