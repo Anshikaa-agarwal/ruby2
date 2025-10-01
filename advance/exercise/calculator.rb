d7aa223e33f798e8822a80223bb1740b96538371
Anshika <anshika2020ag@gmail.com>
2025-10-01 18:34:34 +0530

    converted to float and then integer if int

--- advance/exercise/calculator.rb
+++ advance/exercise/calculator.rb
@@ -1,44 +1,56 @@
 # frozen_string_literal: true
 
-module Convertor
-  def str_to_sym(str)
-    str.tr(':', '').to_sym
-  end
-  def str_to_int(str)
-    str.to_i
-  end
-end
-
-module Calculation
-  def arithmetic_op(first, operator, second)
-    first.send(operator, second)
+# convert float to integer if number is integer
+class Float
+  def convert_if_integer
+    temp = Integer(self)
+    self == temp ? temp : self
   end
 end
 
-class Evaluator
-  include Convertor
-  include Calculation
+# class to evalute string
+class Calculator
+  attr_reader :first, :operand, :second
+  VALID_OPERANDS = [:+, :-, :/, :*, :**]
 
-  def parse_inputs(input)
-    first_str, op_str, second_str = input.split(",")
-    
+  def initialize(*args)
+    raise ArgumentError, "Expected 3 arguments: number, operator, number" unless args.size == 3
 
-  def calculate(**args)
-    # first_str, op_str, second_str = input.split(",")
-    # first  = str_to_int(first_str)
-    # second = str_to_int(second_str)
-    # operator = str_to_sym(op_str)
+    @first = parse_number(args[0])
+    @operand = parse_operand(args[1])
+    @second = parse_number(args[2])
+  end
 
-    # arithmetic_op(first, operator, second)
+  def evaluate
+    result = first.public_send(operand, second)
+    raise ZeroDivisionError, "Can not divide by 0" if second.zero? && operand == :/
 
-    
+    result.convert_if_integer
+  end
+
+  private
+
+  def parse_number(num)
+    Float(num)
+  rescue ArgumentError
+    raise ArgumentError, "Input must be Numeric"
+  end
+
+  def parse_operand(val)
+    sym = val.tr(':', '').to_sym
+    raise ArgumentError, "Operand must be #{VALID_OPERANDS}" unless VALID_OPERANDS.include?(sym)
+    sym
   end
 end
 
 if ARGV.empty?
   puts 'Please provide an input'
 else
-  evaluator = Evaluator.new
-  input = ARGV[0]
-  puts evaluator.calculate(input)
+  begin
+    inputs = ARGV[0].split(',').map(&:strip)
+    calc1 = Calculator.new(*inputs)
+    puts calc1.evaluate
+  rescue StandardError => e
+    puts "#{e.message} (#{e.class})"
+  end
 end
